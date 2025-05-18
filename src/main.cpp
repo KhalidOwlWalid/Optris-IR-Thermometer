7330c5182a978499ecc5ebe02fe79904da3f5e88
KhalidOwlWalid <khalidowlwalid@gmail.com>
2025-05-18 23:37:32 +0100

    [feat] Working function to read the data

--- src/main.cpp
+++ src/main.cpp
@@ -11,19 +11,23 @@ class Optris_IR_Thermometer: public SoftwareSerial {
 
     void decode_process_temperature();
 
-    byte get_sync_byte() { return _sync_byte; }
-    byte get_end_byte() { return _end_byte; }
-  
+    static const byte sync_byte = 0xAA;
+    static const byte end_byte = 0x00;
+
+    // Frame size which contains useful data consists of 14 bytes with another 4 bytes being
+    // sync frame and end frame with 2 bytes for each frame
+    static const size_t expected_frame_size = 18;
+ 
   private:
     bool _initialized;
+    bool _read_frame;
     uint8_t _rx_pin;
     uint8_t _tx_pin;
     unsigned long _baudrate;
+    
+
     char _buffer[14];
     float _process_temperature;
-
-    byte _sync_byte = 0xAA;
-    byte _end_byte = 0x00;
     
     // Not implemented
     float _head_temperature;
@@ -31,6 +35,9 @@ class Optris_IR_Thermometer: public SoftwareSerial {
     float curr_ambient_temperature;
     float _curr_emmisivity;
 };
+
+// Shortening the alias to the Optris class
+using Thermometer = Optris_IR_Thermometer;
 
 // Since the IR thermometer works on RS232, an RS232-TTL converter is required
 // for the voltage logic level to work with the software serial
@@ -57,10 +64,48 @@ void setup() {
 }
 
 void loop() {
-  if (ir_thermometer_1.available()) {
-    int byte_val = ir_thermometer_1.read();
-    Serial.print("Byte value: ");
-    Serial.println(byte_val, HEX);
+  if (ir_thermometer_1.available() > Thermometer::expected_frame_size) {
+    // We would only want to read when we receive the sync bytes 0xAAAA
+    bool read_frame = false;
+    int sync_frame_count = 2;
+    if (ir_thermometer_1.peek() == 0xAA) {
+      // Remove the sync byte frame from the buffer by reading it
+      while (sync_frame_count > 0) {
+        if (ir_thermometer_1.read() == 0xAA) {
+          // Decrement the count if we found the sync frame
+          sync_frame_count--;
+        } else {
+          // If the byte found is not the sync frame, then assume framing error 
+          break;
+        }
+        
+        if (sync_frame_count == 0) {
+          // Once the sync frame reaches zero, simply lock the loop to read the buffer
+          read_frame = true;
+          // Break out of the while loop to start reading the data
+          break;
+        }
+      }
+
+      while (read_frame) {
+        // TODO (Khalid): It is likely that one of the bytes are actually 0x00, so make sure
+        // to check two bytes of 0x00
+        if (ir_thermometer_1.peek() == 0x00) {
+          read_frame = false;
+          // Just break out of the loop if we know it is the end of the frame
+          break;
+        }
+        Serial.print("Current byte value is: ");
+        Serial.print(ir_thermometer_1.read(), HEX);
+        Serial.println("");
+      }
+
+    } else {
+      Serial.print("Flushing the useless byte: ");
+      Serial.print(ir_thermometer_1.read(), HEX);
+      Serial.println("");
+    }
+
   }
   delay(50);
 }
