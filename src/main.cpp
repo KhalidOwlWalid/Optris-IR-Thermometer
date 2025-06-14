7df5b6d7f4caa338422aa635a99c8e5d07abf8ee
KhalidOwlWalid <khalidowlwalid@gmail.com>
2025-06-14 17:32:46 +0100

    [refactor] Remove the use of delay()

--- src/main.cpp
+++ src/main.cpp
@@ -10,30 +10,36 @@
 Optris_IR_Thermometer ir_thermometer_1(8, 7);
 static unsigned long last_update_ms;
 
+bool initialized = false;
+
 void setup() {
 
   last_update_ms = millis();
+  static unsigned long last_retry_ms = millis();
   Serial.begin(9600);
   
   // Initialize the baudrate of the IR thermometer with 9600 baudrate
   // The RS232 is also inverted
   // Sensor will attempt to initialize a few times, if failed then just continue
   size_t n_init_attempt = MAX_N_ATTEMPT;
-  while (n_init_attempt > 0) {
-    if (ir_thermometer_1.begin(SENS_BAUDRATE, SERIAL_8N1, true)) {
-      Serial.println("IR Thermometer Serial initialized!");
-    } else {
-      Serial.println("Fails to initialize the IR thermometer sensor. Retrying...");
-      n_init_attempt--;
-      // TODO (Khalid): Figure out a reasonable delay value
-      delay(1000);
+  while (not initialized) {
+    unsigned long now_ms = millis();
+    if (now_ms - last_retry_ms > 1000) {
+      if (ir_thermometer_1.begin(SENS_BAUDRATE, SERIAL_8N1, true)) {
+        Serial.println("IR Thermometer Serial initialized!");
+        initialized = true;
+      } else {
+        Serial.println("Fails to initialize the IR thermometer sensor. Retrying...");
+        last_retry_ms = millis();
+      }
     }
+    delay(1000);
   }
 }
 
 
 void loop() {
-  float data = ir_thermometer_1.request_data(CMD_READ_PROCESS_TEMP);
+  float data = ir_thermometer_1.request_data(CMD_READ_HEAD_TEMP);
   Serial.println(data);
   delay(10);
 }
